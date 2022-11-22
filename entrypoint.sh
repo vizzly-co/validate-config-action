2180187e7d064a2b933c9ef09e738cce9354e69e
James Bowers <james@vizzly.co>
2022-11-22 15:01:26 +0000

    bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,3 @@
 #!/bin/sh
 
-echo "Am i seen?"
-echo "Am i seen? 2"
-
-
-vizzly --help
-
-echo "Starting validating..."
-
-vizzly validate
-
-vizzly validate --config "$1" >> out
-
-cat out
-
-vizzly validate --config "$1" && echo "Done."
-
-pwd
-ls ./
-ls /
-
-# `$*` expands the `args` supplied in an `array` individually
-# or splits `args` in a string separated by whitespace.
-sh -c "echo $*"
-
-node /writeSomething.js
-
+vizzly validate --config "$1"
--- writeSomething.js
+++
@@ -1,5 +0,0 @@
-
-console.log("Printed using console.log")
-console.info("Printed using console.info")
-
-process.stdout.write('Printed using stdout.write')
