2f440d29df5595f7f2a3fa9a5ac0916c1fb158c5
James Bowers <james@vizzly.co>
2022-11-22 14:23:39 +0000

    use dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM node:16-alpine
 
-WORKDIR /var/app
-
 RUN npm i -g @vizzly/cli
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
-#!/bin/sh -l
+#!/bin/sh
 
 echo "Starting validating..." >> $GITHUB_OUTPUT
 
 vizzly validate --config "$1" >> $GITHUB_OUTPUT
+
+# `$*` expands the `args` supplied in an `array` individually
+# or splits `args` in a string separated by whitespace.
+sh -c "echo $*" >> $GITHUB_OUTPUT
